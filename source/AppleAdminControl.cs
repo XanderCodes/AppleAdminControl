e132e95fb35b2d20a8cff068cb19dfc77190a223
AppleManYT <xanderstuff7@gmail.com>
2021-12-16 03:52:28 +1100

    Updated text and added move-only-owned barricades and structures
    Updated text and added move-only-owned barricades and structures.
    
    Updated assembly version.

--- source/AppleAdminControl.cs
+++ source/AppleAdminControl.cs
@@ -3,7 +3,11 @@ using Rocket.Core.Logging;
 using Rocket.Core.Plugins;
 using Rocket.Unturned;
 using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
 using System;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
 
 namespace AppleAdminControl
 {
@@ -12,14 +16,23 @@ namespace AppleAdminControl
         protected override void Load()
         {
             Logger.Log($"AppleAdminControl version {Assembly.GetName().Version} has been loaded!", ConsoleColor.Magenta);
-            Logger.Log($"AppleAdminControl created by AppleManYT#8750, for use on RedstonePlugins.com!", ConsoleColor.Cyan);
+            Logger.Log($"AppleAdminControl created by AppleManYT#8750 - For support, contact me via the UnturnedStore.com Discord, or submit a GitHub issue!", ConsoleColor.Cyan);
 
             U.Events.OnPlayerConnected += OnPlayerConnect;
             U.Events.OnPlayerDisconnected += OnPlayerDisconnect;
+
+            BarricadeManager.onTransformRequested += OnBarricadeTransformRequested;
+            StructureManager.onTransformRequested += OnStructureTransformRequested;
         }
 
         protected override void Unload()
         {
+            U.Events.OnPlayerConnected -= OnPlayerConnect;
+            U.Events.OnPlayerDisconnected -= OnPlayerDisconnect;
+
+            BarricadeManager.onTransformRequested -= OnBarricadeTransformRequested;
+            StructureManager.onTransformRequested -= OnStructureTransformRequested;
+
             Logger.Log($"{Name} has been unloaded!", ConsoleColor.Magenta);
         }
 
@@ -28,19 +41,27 @@ namespace AppleAdminControl
             if (player.HasPermission("admin.freecam"))
             {
                 player.Player.look.sendFreecamAllowed(true);
+            } 
+            else
+            {
+                player.Player.look.sendFreecamAllowed(false);
             }
-            else if (player.HasPermission("admin.editor"))
+
+            if (player.HasPermission("admin.editor"))
             {
                 player.Player.look.sendWorkzoneAllowed(true);
             }
-            else if (player.HasPermission("admin.spectate"))
+            else
+            {
+                player.Player.look.sendWorkzoneAllowed(false);
+            }
+
+            if (player.HasPermission("admin.spectate"))
             {
                 player.Player.look.sendSpecStatsAllowed(true);
             }
             else
             {
-                player.Player.look.sendFreecamAllowed(false);
-                player.Player.look.sendWorkzoneAllowed(false);
                 player.Player.look.sendSpecStatsAllowed(false);
             }
         }
@@ -50,6 +71,53 @@ namespace AppleAdminControl
             player.Player.look.sendFreecamAllowed(false);
             player.Player.look.sendWorkzoneAllowed(false);
             player.Player.look.sendSpecStatsAllowed(false);
+        }
+
+        private void OnBarricadeTransformRequested(CSteamID instigator, byte x, byte y, ushort plant, uint instanceID, ref Vector3 point, ref byte angle_x, ref byte angle_y, ref byte angle_z, ref bool shouldAllow)
+        {
+            if (BarricadeManager.tryGetRegion(x, y, plant, out BarricadeRegion region))
+            {
+                foreach (BarricadeDrop drop in region.drops)
+                {
+                    if (drop.instanceID == instanceID)
+                    {
+                        ulong owner = drop.GetServersideData().owner;
+
+                        CSteamID ownerID = (CSteamID)owner;
+                        if (ownerID != instigator)
+                        {
+                            if (!UnturnedPlayer.FromCSteamID(instigator).HasPermission("admin.editor.otherobjects"))
+                            {
+                                shouldAllow = false;
+                                ChatManager.serverSendMessage("You can only move your own barricades!!", Color.red, null, UnturnedPlayer.FromCSteamID(instigator).SteamPlayer(), EChatMode.SAY, null, true);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnStructureTransformRequested(CSteamID instigator, byte x, byte y, uint instanceID, ref Vector3 point, ref byte angle_x, ref byte angle_y, ref byte angle_z, ref bool shouldAllow)
+        {
+            foreach (StructureRegion region in StructureManager.regions)
+            {
+                foreach (StructureDrop drop in region.drops)
+                {
+                    if (drop.instanceID == instanceID)
+                    {
+                        ulong owner = drop.GetServersideData().owner;
+                        CSteamID ownerID = (CSteamID)owner;
+                        if (ownerID != instigator)
+                        {
+                            if (!UnturnedPlayer.FromCSteamID(instigator).HasPermission("admin.editor.otherobjects"))
+                            {
+                                shouldAllow = false;
+                                ChatManager.serverSendMessage("You can only move your own structures!!", Color.red, null, UnturnedPlayer.FromCSteamID(instigator).SteamPlayer(), EChatMode.SAY, null, true);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
